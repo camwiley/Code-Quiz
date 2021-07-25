8cd441370e477315385b369a558d6774c0e2bd44
Cam Wiley <insuredbycam@gmail.com>
2021-07-25 12:31:42 -0400

    Finalized html  elements for application

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var startQuiz = document.querySelector('#start-btn');
+
+var questions = [
+    {
+        title: "Which of the following are not data types? ",
+        choices: ["booleans", "integers", "numbers", "arrays"],
+        answers: "arrays"
+    },
+    {
+        title: "What kind of statement is used to execute actions based on a trigger or condition?",
+        choices: ["conditional statement","booleans","expression", "fired event"],
+        answers: "conditional statement"
+    },
+    {
+        title: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        choices: ["repeater", "loop", "debugger", "iteration"],
+        answers: "loop"
+    },
+    {
+        title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        choices: ["else loop", "while loop", "for loop", "conditional loop"],
+        answers: "while loop"
+    },
+    {
+        title: "What elements are used to test for TRUE or False values stored in variables?",
+        choices: ["comparison and logical operators", "conditional statements", "trigger events", "expressions"],
+        answers: "comparison and logical operators"
+    }
+]
+var quizQuestions = function() {
+    for (i=0 , i <)
+}
+
+]
