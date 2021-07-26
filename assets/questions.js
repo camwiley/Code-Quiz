0dde5a0578d7477ee41df054488b9299664d9a48
Cam Wiley <insuredbycam@gmail.com>
2021-07-26 00:00:00 -0400

    finished app

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+        question: "Which of the following are not data types? ",
+        answer: "arrays",
+        choices: [
+            "booleans", 
+            "integers", 
+            "numbers", 
+            "arrays"
+        ],
+    },
+
+    {
+        question: "What kind of statement is used to execute actions based on a trigger or condition?",
+        answer: "conditional statement",
+        choices: [
+            "conditional statement",
+            "booleans",
+            "expression", 
+            "fired event"
+        ],  
+    },
+
+    {
+        question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        answer: "loop",
+        choices: [
+            "repeater", 
+            "loop", 
+            "debugger", 
+            "iteration"
+        ],
+    },
+
+    {
+        question: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        answer: "while loop",
+        choices: [
+            "else loop", 
+            "while loop", 
+            "for loop", 
+            "conditional loop"
+        ],
+        
+    },
+
+    {
+        question: "What elements are used to test for TRUE or False values stored in variables?",
+        answer: "comparison and logical operators",
+        choices: [
+            "comparison and logical operators", 
+            "conditional statements", 
+            "trigger events", 
+            "expressions"
+        ],
+    }
+]
